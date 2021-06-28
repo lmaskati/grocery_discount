b70b00bc744d77f7f7c47403446f0f9fe415bc28
lmaskati <liyaan_maskati@brown.edu>
2021-06-28 16:12:50 -0400

    event handling begins woo

--- discount_interface/src/App.js
+++ discount_interface/src/App.js
@@ -6,8 +6,18 @@ import { Spinner, Row } from 'react-bootstrap';
 import HeaderBar from './HeaderBar';
 import SupermarketCard from './SupermarketCard';
 
+
+
+
 const App = () => {
+
   const [content, setContent] = useState(undefined);
+
+  function handleClick() {
+    setContent(undefined);
+  
+    console.log("fffff");
+  }
 
   useEffect(async () => {
     const fetchList = async () => {
@@ -22,7 +32,7 @@ const App = () => {
         return;
       }
       const json = await result.json();
-      setContent(json);
+      setContent("hello");
     };
     fetchList();
   }, []);
@@ -43,25 +53,18 @@ const App = () => {
     const shopNames = [["Fair Price", "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco/v1436334225/p4ucgwxnao0czp2ce1ap.png"], ["Cold Storage", "https://yt3.ggpht.com/ytc/AAUvwnirVQw7soAGlGXHSXBcRf5eAIae_lVAKcPQJ1vB=s900-c-k-c0x00ffffff-no-rj"]]
     stuff = (
       <div className="w-100 h-100 d-flex align-items-center justify-content-center">>
+       
         <Row xs={1} md={10} className="g-4 d-flex justify-content-center">
     {shopNames.map((item) => <SupermarketCard name={item[0]} link={item[1]} />)}
     </Row>
       </div>
     )
 
-    // stuff = (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       {content.map((item) => <Supermarket name={item} />)}
-    //       <p>The current time is 5.</p>
-    //     </header>
-    //   </div>
-    // );
   }
 
   return (
     <div className="d-flex flex-column w-100 h-100">
-      <HeaderBar />
+      <HeaderBar handle={handleClick}/>
       <div className="d-flex flex-grow-1">
         {stuff}
       </div>
--- discount_interface/src/HeaderBar.js
+++ discount_interface/src/HeaderBar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import { Navbar, Nav, FormControl, Button, Form } from 'react-bootstrap';
- 
-const HeaderBar = () => (
+
+
+
+
+const HeaderBar = ({handle}) => (
+
   <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Liyaan's Cool Grocery Site</Navbar.Brand>
+    <Navbar.Brand onClick={()=> {handle()}}>Liyaan's Cool Grocery Site</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Store 1</Nav.Link>
-      <Nav.Link href="#features">Store 2</Nav.Link>
-      <Nav.Link href="#pricing">Store 3</Nav.Link>
+      <Nav.Link onClick={()=> console.log("Fair Price")}>Fair Price</Nav.Link>
+      <Nav.Link onClick={()=> console.log("Cold Storage")}>Cold Storage</Nav.Link>
+      
     </Nav>
     
   </Navbar>
